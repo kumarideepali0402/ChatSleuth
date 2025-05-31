7752dc4e998da290850864f6be724717044790ee
Deepali <deepalikumari62004@gmail.com>
2025-05-31 20:15:25 +0530

    updates

--- core/highlight.py
+++ core/highlight.py
@@ -1,48 +1,104 @@
-def highlight_matches(text: str, pattern: str) -> str:
+# def highlight_matches(text: str, pattern: str) -> str:
+#     if not pattern:
+#         return text
+
+#     lower_text = text.lower()
+#     lower_pattern = pattern.lower()
+
+#     lps = [0] * len(lower_pattern)
+#     i = 1
+#     while i < len(lower_pattern):
+#         if lower_pattern[i] == lower_pattern[length]:
+#             length += 1
+#             lps[i] = length
+#             i += 1
+#         else:
+#             if length != 0:
+#                 length = lps[length - 1]
+#             else:
+#                 lps[i] = 0
+#                 i += 1
+
+#     i = j = 0
+#     match_starts = []
+#     while i < len(lower_text):
+#         if lower_pattern[j] == lower_text[i]:
+#             i += 1
+#             j += 1
+#         if j == len(lower_pattern):
+#             match_starts.append(i - j)
+#             j = lps[j - 1]
+#         elif i < len(lower_text) and lower_pattern[j] != lower_text[i]:
+#             if j != 0:
+#                 j = lps[j - 1]
+#             else:
+#                 i += 1
+
+#     chars = list(text)
+#     for start in reversed(match_starts):
+#         end = start + len(pattern)
+#         chars.insert(end, "</mark>")
+#         chars.insert(start, '<mark style="color: black;">')
+
+#     return ''.join(chars)
+
+
+def highlight_matches(text : str, pattern : str) -> str:
+
     if not pattern:
         return text
-
-    # Case-insensitive match
-    lower_text = text.lower()
+    
     lower_pattern = pattern.lower()
+    lower_text = text.lower()
 
-    # Build LPS array for KMP
     lps = [0] * len(lower_pattern)
-    length = 0  # length of the previous longest prefix suffix
-    i = 1
-    while i < len(lower_pattern):
-        if lower_pattern[i] == lower_pattern[length]:
-            length += 1
-            lps[i] = length
+    i = 0
+    j = 1
+    lps[0] = 0
+    while (j < len(lower_pattern)):
+        if (lower_pattern[j] == lower_pattern[i]) :
+            lps[j] = i + 1
+            j += 1
             i += 1
-        else:
-            if length != 0:
-                length = lps[length - 1]
+        else : 
+            if i == 0:
+                lps[j] = 0
+                j += 1
             else:
-                lps[i] = 0
-                i += 1
-
-    # Find all match indices using KMP
-    i = j = 0
-    match_starts = []
-    while i < len(lower_text):
-        if lower_pattern[j] == lower_text[i]:
+                i = lps[i - 1]
+    
+    i = 0
+    j = 0
+    starting_indices = []
+    while (i < len(lower_text) ) :
+        if (lower_pattern[j] == lower_text[i]) :
             i += 1
             j += 1
-        if j == len(lower_pattern):
-            match_starts.append(i - j)
-            j = lps[j - 1]
-        elif i < len(lower_text) and lower_pattern[j] != lower_text[i]:
-            if j != 0:
+            if (j == len(lower_pattern)) :
+                starting_indices.append(i - j) 
                 j = lps[j - 1]
-            else:
+
+        else:
+            if (j == 0):
                 i += 1
-
-    # Highlight all matches (insert tags in reverse to not mess up positions)
+            else:
+                j = lps[j - 1]
+    
     chars = list(text)
-    for start in reversed(match_starts):
-        end = start + len(pattern)
-        chars.insert(end, "</mark>")
-        chars.insert(start, '<mark style="color: black;">')
-
+    
+    for start in reversed(starting_indices) :
+        chars.insert(start+ len(lower_pattern),"</mark>")
+        chars.insert(start,'<mark style=" color: black;">')
+    
     return ''.join(chars)
+
+
+
+         
+
+    
+
+
+
+    
+
--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -3,33 +3,25 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Download once only (handled in main.py, not here)
-# nltk.download('punkt')
-# nltk.download('stopwords')
 
-# --- Text Preprocessing Steps ---
 
 def text_lowercase(text):
-    """Convert text to lowercase."""
+   
     return text.lower()
 
 def remove_punctuation(text):
-    """Remove punctuation from the text."""
     return text.translate(str.maketrans('', '', string.punctuation))
 
 def remove_whitespace(text):
-    """Remove extra spaces from the text."""
     return " ".join(text.split())
 
 def remove_stopwords(text):
-    """Remove stopwords using NLTK."""
     stop_words = set(stopwords.words("english"))
     word_tokens = word_tokenize(text)
     filtered_text = [word for word in word_tokens if word not in stop_words]
     return " ".join(filtered_text)
 
 def text_preprocess(text):
-    """Full preprocessing pipeline: lowercase, clean, stopword removal."""
     text = text_lowercase(text)
     text = remove_punctuation(text)
     text = remove_whitespace(text)
